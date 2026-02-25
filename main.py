c309e7ff6305ebd09452095d842deb0f2639c995
DiggoSants <diegoofc135@gmail.com>
2026-02-24 21:01:44 -0300

    Add: start increment

--- main.py
+++ main.py
@@ -10,35 +10,72 @@ jogador = {
     "ouro": 0
 }
 
-def SAVE_GAME():
+def saveGame():
     with open("save.json","w") as f:
         json.dump(jogador,f,indent=4)
 
-def LOAD_GAME():
+def loadGame():
     global jogador
     if os.path.exists("save.json"):
         with open("save.json","r") as f:
             jogador = json.load(f)
 
-SAVE_GAME()
-print("Jogo salvo com sucesso!")
+def deleteSave():
+    if os.path.exists("save.json"):
+        os.remove("save.json")
+        print("Salvamento excluído com sucesso.")
+    else:
+        print("Nenhum salvamento encontrado.")
+
+def showCredits():
+    print("Créditos do Jogo:")
+    print("Desenvolvedor: DiggoSants")
+    print("Versão: 1.0")
+    print("Obrigado por jogar!")
+    print("1 - voltar")
+
+    select = input("Ação: ")
+    if select == "1":
+        startGame()
+    else:
+        print("Opção inválida. Tente novamente.")
+        showCredits()
+
+def startRound():
+    print("Iniciando nova rodada...")
+    # Lógica da rodada aqui
+    jogador
 
 # Jogar
-def menu_Principal():
+def startGame():
     print("Bem-vindo ao jogo!")
     print("1 - Começar")
     print("2 - Customizar")
     print("3 - Créditos")
-    print("4 - Sair")
+    print("4 - Fechar")
 
-    escolha = input("Ação: ")
-    if escolha == "1":
-        print("Iniciando o jogo...")
-    elif escolha == "2":
+    select = input("Ação: ")
+    if select == "1":
+        startRound()
+
+    elif select == "2":
         print("Customizando o personagem...")
-    elif escolha == "3":
-        print("Créditos do jogo...")
-    elif escolha == "4":
+        # customizeCharacter()
+    elif select == "3":
+        print("Carregando os créditos do jogo...")
+        showCredits()
+
+    elif select == "4":
         print("Saindo do jogo...")
-    
-menu_Principal()
+        endGame()
+    else:
+        print("Opção inválida. Tente novamente.")
+        startGame()
+
+def endGame():
+    print("Fim de jogo! Obrigado por jogar.")
+    exit()
+
+
+
+startGame()
